b591ea91f6ea3ef6bdf20c272cd86dcb38524da0
Sean Rudd <seanmrudd@me.com>
2019-04-03 17:35:21 -0400

    improved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,6 @@ var letterBank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
 var wins = 0;
 var losses = 0;
 
-// What gets displayed onto the page
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesLeft-text");
-var guessesSoFarText = document.getElementById("guessesSoFar-text");
-
 // When button is clicked, gameStart function starts
 function gameStart() {
 
@@ -24,54 +18,64 @@ function gameStart() {
     var computerLetterPick = letterBank[Math.floor(Math.random() * letterBank.length)];
     console.log(computerLetterPick);
 
-
+    function restart() {
+        guessesLeft = 13;
+        guessesArray = [];
+    }
 
 
     //keyup is stored as userGuess
     document.onkeyup = function () {
-        var userGuess = event.key;
-        console.log(userGuess)
+        if (event.keyCode > 64 && event.keyCode < 91) {
+            var userGuess = event.key.toLowerCase();
+            console.log(userGuess)
 
-        // If the userGuess and computer random number are identical, then wins is added and it loops back to the top of the function with gameStart
-        if (userGuess === computerLetterPick) {
-            wins++;
-            guessesLeft = 13;
-            guessesArray = [];
-            gameStart();
-        } else {
-            // if the userGuess doesn't match the computer random number, the amount of guesses left goes down
-            guessesLeft--;
-            // userGuess is also assigned to wrongGuess
-            var wrongGuess = userGuess;
-            // wrongGuess is then pushed to the guessesArray
-            guessesArray.push(wrongGuess)
+            // If the userGuess and computer random number are identical, then wins is added and it loops back to the top of the function with gameStart
+            if (!guessesArray.includes(userGuess)) {
+                if (userGuess === computerLetterPick) {
+                    wins++;
+                    restart();
+                    gameStart();
+                } else {
+                    // if the userGuess doesn't match the computer random number, the amount of guesses left goes down
+                    guessesLeft--;
+                    // userGuess is also assigned to wrongGuess
+                    var wrongGuess = userGuess;
+                    // wrongGuess is then pushed to the guessesArray
+                    guessesArray.push(wrongGuess)
 
-            // if the user runs out of guesses, a loss is added and the game will start over
-            if (guessesLeft === 0) {
-                losses++;
-                guessesLeft = 13;
-                guessesArray = [];
-                gameStart();
+                    // if the user runs out of guesses, a loss is added and the game will start over
+                    if (guessesLeft == 0) {
+                        losses++;
+                        restart();
+                        gameStart();
+                    }
+                }
+
+                // a value is added to string
+                var guessesSoFar = ""
+                // for loop for array
+                for (i = 0; i < guessesArray.length; i++) {
+                    //the wrong guess that went into the array is assigned to variable wrong letter
+                    var wrongLetter = guessesArray[i];
+                    // the total guesses so far is assigned to the string that has the wrong letter plus all the other previous wrong guesses
+                    guessesSoFar += " " + wrongLetter;
+                    console.log(guessesSoFar)
+                }
+
+                // What gets displayed onto the page
+                document.getElementById("wins-text").textContent = "Wins: " + wins;
+                document.getElementById("losses-text").textContent = "Losses: " + losses;
+                document.getElementById("guessesLeft-text").textContent = "Guesses left: " + guessesLeft;
+                document.getElementById("guessesSoFar-text").textContent = "Guesses so far: " + guessesSoFar;
+
             }
-
         }
-
-        // a value is added to string
-        var guessesSoFar = ""
-        // for loop for array
-        for (i = 0; i < guessesArray.length; i++) {
-            //the wrong guess that went into the array is assigned to variable wrong letter
-            var wrongLetter = guessesArray[i];
-            // the total guesses so far is assigned to the string that has the wrong letter plus all the other previous wrong guesses
-            guessesSoFar += " " + wrongLetter;
-            console.log(guessesSoFar)
-        }
-
-
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        guessesLeftText.textContent = "Guesses left: " + guessesLeft;
-        guessesSoFarText.textContent = "Guesses so far: " + guessesSoFar;
-        
     }
 }
+// .push(userGuess);
+//.textContent [joined-function]
+//if(event.keyCode > 64 && event.keyCode < 91)
+//if(event.key >= 'a' && event.key >= 'z')
+//if(!guessedLetters.includes(userGuess)){}  ------- !=if not
+//if(guessedLetters.indexOf(userGuess) = -1
